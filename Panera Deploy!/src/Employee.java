5e5efdbff70946665d7cbc27fd2280b7dc32356e
MRBeussink <MRBeussink@gmail.com>
2016-03-16 16:30:01 -0500

    First Commit!
    Added unfinished Employee and Time classes

---
+++ Panera Deploy!/src/Employee.java
@@ -0,0 +1,23 @@
+/* This class repersents an employee
+ * 
+ */
+
+
+public class Employee {
+	
+	private String name;
+	private boolean[] skillSet;
+	
+	private class SkillSet {
+		private boolean register;
+		private boolean production;
+		private boolean diningRoom;
+		
+		private int numOfSkills;
+		
+		public SkillSet(boolean register, boolean production, boolean diningRoom){
+			
+		}
+	}
+
+}
