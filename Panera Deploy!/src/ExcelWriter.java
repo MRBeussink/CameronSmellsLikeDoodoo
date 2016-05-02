77e25193e867fc382538e805cf959f557abafc2b
MRBeussink <MRBeussink@gmail.com>
2016-05-02 08:38:55 -0500

    Even better

--- Panera Deploy!/src/ExcelWriter.java
+++ Panera Deploy!/src/ExcelWriter.java
@@ -16,28 +16,37 @@ public class ExcelWriter {
 	
 	public void writeDoc(ArrayList<Position> positions) throws FileNotFoundException{
 		try {
+			if(Driver.test)
+				System.out.println("Opeening input stream");
 			FileInputStream fsIP = new FileInputStream("Wednesday.xls");
 			try {
+				if(Driver.test)
+					System.out.println("Creating new workbook");
 				HSSFWorkbook workbook = new HSSFWorkbook(fsIP);
 				//workbook.getSheetAt(x);
 				for (int i = 0; i < positions.size(); i++){
-					int rownum = (int) positions.get(i).getRow();
-					int colnum = (int) positions.get(i).getCol();
+					int rownum = (int) positions.get(i).getRow() - 1;
+					int colnum = (int) positions.get(i).getCol() - 1;
 					int pagenum = (int) positions.get(i).getPage();
 
 					//write to the page
 					if(Driver.test)
 						System.out.println("Writing employee " + positions.get(i).getEmployeeName() + " to " +
-						pagenum + ", " + rownum +", " + ", " + colnum);
+						pagenum + ", " + rownum +", " + colnum);
 					HSSFSheet worksheet = workbook.getSheetAt(pagenum);
 					Cell cell = null;
 					cell = worksheet.getRow(rownum).getCell(colnum);
+					cell.setCellValue("ERROR");
 					cell.setCellValue(positions.get(i).getEmployee().toString());
+					//FileOutputStream output = new FileOutputStream("Wednesday.xls");
 				}
 				fsIP.close();
+
 				FileOutputStream output = new FileOutputStream("Wednesday.xls");
 				workbook.write(output);
 				output.close();
+
+				//output.close();
 
 				/*
 				while (!position.isEmpty()) {
@@ -51,7 +60,7 @@ public class ExcelWriter {
 			}
 		}
 		catch (FileNotFoundException e){
-			System.out.println("Unable to find output filed.");
+			System.out.println("Unable to find output file.");
 		}
 	}
 }
